4c670565f9b919a5dd3656795da1182f8d5a158f
RMS-Kimizuto <lgtan47@gmail.com>
2023-11-26 10:30:07 +0700

    Rermove frame debug code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-//#include "clock/console_clock.cpp"
-#include <iostream>
-using namespace std;
-
-#include "graphic/frame/frame.cpp"
+#include "clock/console_clock.cpp"
 
 int main()
 {
-    //console_graphic_clock();
-    Frame f = getFrameFromNumber(TWO);
-    cout << "Frame width: " << f.width << endl;
-    cout << "Frame height: " << f.height << endl;
-    printFrameToScreen(f);
+    console_graphic_clock();
     return 0;
 }
